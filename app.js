e1cf11b194e8b3aac430738dd5d1a08c2e0e70c6
Tervicke <ameywpatil@gmail.com>
2022-05-24 22:47:27 +0530

    new UI v2

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-let isDarkMode = false;
-function switchMode(){
-	if(isDarkMode){
-		isDarkMode=false;
-		document.getElementsByTagName("body")[0].style.backgroundColor = '#eee';
-		document.getElementsByTagName("body")[0].style.color= '#121212';
-		let a_tags = document.getElementsByTagName("a");
-		for(const a of a_tags){
-			a.style.color = '#121212';
-		}
-		document.getElementById('switch-mode-button').src = "images/moon.png"
-		document.getElementById('twitter').src='images/twitter_dark.png'
-		document.getElementById('instagram').src='images/instagram_dark.png'
-		document.getElementById('github').src='images/github_dark.png'
-		document.getElementById('discord').src='images/discord_dark.png'
+function LightMode(){
+	document.getElementsByTagName("body")[0].style.backgroundColor = '#eee';
+	document.getElementsByTagName("body")[0].style.color= '#121212';
+	let a_tags = document.getElementsByTagName("a");
+	for(const a of a_tags){
+		a.style.color = '#121212';
 	}
-	else{
-		isDarkMode=true;
-		document.getElementsByTagName("body")[0].style.backgroundColor = '#121212';
-		document.getElementsByTagName("body")[0].style.color= '#E6E6FA';
-		let a_tags = document.getElementsByTagName("a");
-		for(const a of a_tags){
-			a.style.color = '#E6E6FA';
-		}
-		document.getElementById('switch-mode-button').src = "images/sun.png"
-		document.getElementById('twitter').src='images/twitter_light.png'
-		document.getElementById('instagram').src='images/instagram_light.png'
-		document.getElementById('github').src='images/github_light.png'
-		document.getElementById('discord').src='images/discord_light.png'
-	}	
+	document.getElementById('twitter').src='images/twitter_dark.png'
+	document.getElementById('instagram').src='images/instagram_dark.png'
+	document.getElementById('github').src='images/github_dark.png'
+	document.getElementById('discord').src='images/discord_dark.png'
+}	
+function DarkMode(){
+	document.getElementsByTagName("body")[0].style.backgroundColor = '#121212';
+	document.getElementsByTagName("body")[0].style.color= '#E6E6FA';
+	let a_tags = document.getElementsByTagName("a");
+	for(const a of a_tags){
+		a.style.color = '#E6E6FA';
+	}
+	document.getElementById('twitter').src='images/twitter_light.png'
+	document.getElementById('instagram').src='images/instagram_light.png'
+	document.getElementById('github').src='images/github_light.png'
+	document.getElementById('discord').src='images/discord_light.png'
 }
